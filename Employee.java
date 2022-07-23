38cd75448300c51e704e66482246b61281b3a023
jiewpassakorn <passakornjiew@gmail.com>
2022-07-23 12:30:01 +0700

    commit

--- Employee.java
+++ Employee.java
@@ -1,3 +1,21 @@
 class Employee{
     // Attribute
+    private String id;
+    private String name;
+    private Double salary;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+    public void displayEmployee(){
+        System.out.println("ID = "+this.id);
+        System.out.println("Name = "+this.name);
+        System.out.println("Salary = "+this.salary);
+    }
 }
---
+++ Main.java
@@ -0,0 +1,21 @@
+import javax.print.event.PrintJobListener;
+
+public class Main{
+    public static void main(String[] args) {
+        
+        Employee e1 = new Employee(); //Create Object
+        e1.setId("1");
+        e1.setName("Passakorn");
+        e1.setSalary(30000.0);
+        e1.displayEmployee();
+
+        Employee e2 = new Employee(); //Create Object
+        e1.setId("2");
+        e1.setName("Athit");
+        e1.setSalary(50000.0);
+
+        
+
+    }
+
+}
